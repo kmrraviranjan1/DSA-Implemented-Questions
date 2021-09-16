7f26587e6217853b275f62b91865fbd0d8d1f62b
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-09-16 13:07:37 +0530

    is empty done

---
+++ LinkedList/isEmpty.js
@@ -0,0 +1,37 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+  isEmpty() {
+    return this.size === 0 ? true : false;
+  }
+  add(data) {
+    const node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.size++;
+  }
+}
+
+const list = new LinkedList();
+
+console.log(list.isEmpty());
+
+list.add(1);
+
+console.log(list.isEmpty());
